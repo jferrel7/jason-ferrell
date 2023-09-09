1cc8211e86b3b44f20cc7c088587016cf30b9232
Jason Ferrell <jason@natural-logic.com>
2023-09-09 11:15:20 -0400

    switch to nextjs

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Jason Ferrell',
+  description: 'The personal and professional website for Jason Ferrell.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
