bf419545e1496dc5ed3af9015326a580ff39b005
Jason Ferrell <jason@natural-logic.com>
2023-09-09 11:16:35 -0400

    switch to nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h2 className="text-xl font-medium mb-8">web architect, music lover and wannabe chef.</h2>
       <h3 className="text-lg mb-0">Current work</h3>
       <p className="max-w-xl mb-8">Building a content authoring app tailored for knowledge management and creating rich UI experiences.</p>
-      <p>Asheville, NC</p>
+      <p>Asheville, NC</p> 
     </main>
   )
 }
