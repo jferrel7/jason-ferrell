481ce5f72dc3b015105a4fb8bd643d9cbdf99228
Jason Ferrell <jason@natural-logic.com>
2023-09-11 10:24:33 -0400

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-16">
-      <h1 className="text-3xl font-semibold mb-1">Jason Ferrell</h1>
-      <h2 className="text-xl font-medium mb-8">web architect, music lover and wannabe chef.</h2>
-      <h3 className="text-lg mb-0 font-medium">Current work</h3>
-      <p className="max-w-xl mb-8">Building a content authoring app tailored for knowledge management and creating rich UI experiences.</p>
-      <p>Asheville, NC</p>  
+      <h1 className="text-3xl font-semibold mb-1 text-">Jason Ferrell</h1>
+      <h2 className="text-xl font-medium mb-8 text-gray-700">web architect, music lover and wannabe chef.</h2>
+      <h3 className="text-lg mb-0 font-medium text-gray-500">Current work</h3>
+      <p className="max-w-xl mb-8">Building a web content authoring app designed to promote organizational learning and knowledge.</p>
+      <p className="text-gray-500">Asheville, NC</p>  
     </main>
   )
 }
