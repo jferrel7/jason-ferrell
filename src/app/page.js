2f1e270f57abbe6eb4f8baa964d33a03430766b9
Jason Ferrell <jason@natural-logic.com>
2023-12-27 17:50:46 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-16">
-      <h1 className="text-3xl font-semibold mb-4 text-gray-700">Jason Ferrell</h1>
+      <h1 className="text-3xl font-semibold mb-2 text-gray-700">Jason Ferrell</h1>
       <h2 className="text-xl font-medium mb-12 text-gray-400">web architect, music lover and wannabe chef.</h2>
-      <h3 className="text-lg mb-4 font-medium text-gray-700">Current work</h3>
+      <h3 className="text-lg mb-2 font-medium text-gray-700">Current work</h3>
       <p className="max-w-xl mb-12 text-gray-400">Building a web content authoring app designed to promote organizational learning and knowledge.</p>
       <ul className="mb-4">
         <li><a href="https://github.com/jferrel7" title="Github" className="text-purple-500">Github</a></li>
