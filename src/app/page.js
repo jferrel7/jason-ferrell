10c7d1c3eaec6280e2db12ee4d3815bc9a90dc83
Jason Ferrell <jason@natural-logic.com>
2023-12-27 17:54:18 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h2 className="text-xl font-medium mb-12 text-gray-400">web architect, music lover and wannabe chef.</h2>
       <h3 className="text-lg mb-2 font-medium text-gray-700">Current work</h3>
       <p className="max-w-xl mb-12 text-gray-400">Building a web content authoring app designed to promote organizational learning and knowledge.</p>
-      <ul className="mb-4">
+      <ul className="mb-12">
         <li><a href="https://github.com/jferrel7" title="Github" className="text-purple-500">Github</a></li>
         <li><a href="https://bsky.app/profile/jasonferrell.bsky.social" title="Bluesky" className="text-purple-500">Bluesky</a></li>
         <li><a href="https://instagram.com/jasonferrell" title="Instagram" className="text-purple-500">Instagram</a></li>
