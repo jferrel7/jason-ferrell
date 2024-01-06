270d7ebf2ff3f77f8f7bd63cd37669b510d29864
Jason Ferrell <jason@natural-logic.com>
2024-01-06 10:50:49 -0500

    analytics

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-16">
       <h1 className="text-3xl font-semibold mb-1 text-gray-700">Jason Ferrell</h1>
-      <h2 className="text-xl font-medium mb-8 text-gray-400">web architect, music lover and wannabe chef.</h2>
+      <h2 className="text-xl font-medium mb-8 text-gray-400">web architect, music lover, and wannabe chef.</h2>
       <h3 className="text-lg mb-0 font-medium text-gray-700">Current work</h3>
       <p className="max-w-xl mb-8 text-gray-400">Building a web content authoring app designed to promote organizational learning and knowledge.</p>
       <ul className="mb-4">
