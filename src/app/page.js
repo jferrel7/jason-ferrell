c2ee89be063b1d81fdf8f804fc841669ff2ed7b8
Jason Ferrell <jason@natural-logic.com>
2025-01-11 19:11:35 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,9 @@ export default function Home() {
       <h1 className="text-3xl font-semibold mb-2 text-gray-700">Jason Ferrell</h1>
       <h2 className="text-xl font-medium mb-12 text-gray-400">web architect, music lover and wannabe chef.</h2>
       <h3 className="text-lg mb-2 font-medium text-gray-700">Current work</h3>
-      <p className="max-w-xl mb-12 text-gray-400">Building a web content authoring app designed to promote organizational learning and knowledge.</p>
+      <p className="max-w-xl mb-12 text-gray-400">
+        Building a knowledge portal for company intranets, with an integrated block editor for creating structured content with curated enterprise video/image/learning media.
+      </p>
       <ul className="mb-12">
         <li><a href="https://github.com/jferrel7" title="Github" className="text-purple-500">Github</a></li>
         <li><a href="https://bsky.app/profile/jasonferrell.bsky.social" title="Bluesky" className="text-purple-500">Bluesky</a></li>
