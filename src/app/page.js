167025e2e3741fbde4164fe7e7a2a5dac108b01e
Jason Ferrell <jason@natural-logic.com>
2025-01-20 14:57:41 -0500

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h2 className="text-xl font-medium mb-12 text-gray-400">web architect, music lover and wannabe chef.</h2>
       <h3 className="text-lg mb-2 font-medium text-gray-700">Current work</h3>
       <p className="max-w-xl mb-12 text-gray-400">
-        Building a knowledge portal for company intranets, with an integrated block editor for creating structured content that includes curated enterprise video, image, learning, and files.
+        Building an enterprise knowledge portal solution, with an integrated block editor for creating structured content. The types of structured content are instruction steps, troubleshooting/how-to info, training curriculum that includes user specific status, trackable action items, and also straight rich text, tables, images, and videos. 
       </p>
       <ul className="mb-12">
         <li><a href="https://github.com/jferrel7" title="Github" className="text-purple-500">Github</a></li>
